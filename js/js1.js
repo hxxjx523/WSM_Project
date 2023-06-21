1d091e6caf745d8f2b06b7e0715c97eaac290b88
heeju <hhj05230@gmail.com>
2023-06-21 15:52:24 +0900

    메인

--- js/js1.js
+++ js/js1.js
@@ -1,6 +1,16 @@
-const toggleBtn = document.querySelector('.nav-toggle');
-const menu = document.querySelector('.nav-list');
+// const navToggleDiv = document.getElementById("nav_toggle");
+const navToggleI = nav_toggle.getElementsByTagName("i")[0];
+const navListUl = document.getElementsByClassName("nav-list")[0];
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('show-menus');
-});
+//2. Event Handling
+// nav_toggle.onclick = () => alert("안녕");
+// nav_toggle.onclick = alert("안녕");
+nav_toggle.onclick = () => {
+    //햄버거메뉴 클릭하면, 햄버거메뉴 i "bi bi-list"<-> X "bi bi-x-lg"
+    navToggleI.classList.toggle("bi-chevron-compact-down");
+    navToggleI.classList.toggle("bi-chevron-compact-up");
+
+    //햄버거메뉴 클릭하면, .nav-list에 .show-munu보여주기 <-> 안보여주기
+    navListUl.classList.toggle("show-menu");
+
+}
